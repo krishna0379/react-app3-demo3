ee6ab410c64a79a68bef23ed5b6fc09b3a767238
krishna0379 <iamkrishna0379@gmail.com>
2021-06-01 22:39:55 +0530

    nav bar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Page3 } from "./pages/page3";
 import { Page2 } from "./pages/page2";
 import { Page1 } from "./pages/Page1";
+import { Navbar } from "react-bootstrap";
 
 function App() {
   return (
     <Router>
+      {/**NAVAIGtion logic  */}
+      <Navbar bg="dark" variant="red">
+        <Navbar.Brand href="#home">Project Book</Navbar.Brand>
+      </Navbar>
       <Switch>
         <Route path="/page1">
           <Page1></Page1>
